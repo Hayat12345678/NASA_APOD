7870f31ec673b880fcb37ad26c350adde0675f80
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-12-22 09:52:18 +0100

    NASA

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
   .then(function (response) {
     // handle success
 
+   
     document.getElementById("title").innerHTML = response.data.title;
+    document.getElementById("datum").innerHTML = response.data.date;
     document.getElementById("beschreibung").innerHTML = response.data.explanation;
     document.getElementById("copyright").innerHTML = response.data.copyright;
-    document.getElementById("datum").innerHTML = response.data.date;
-
     document.getElementById("image").src = response.data.hdurl;
     
     
